3b2bb5682241483ab682cbb66abc32d7721773a0
user.email <lukas.straubing@gmail.com>
2023-05-05 17:47:23 +0200

    first commit

---
+++ src/main/java/wsb2023/pogorzelski/PogorzelskiApplication.java
@@ -0,0 +1,13 @@
+package wsb2023.pogorzelski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogorzelskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogorzelskiApplication.class, args);
+	}
+
+}
