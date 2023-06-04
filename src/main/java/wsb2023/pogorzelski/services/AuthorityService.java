fb6c4168e3a52b78f4f476ceb0cb48533d78c37b
user.email <lukas.straubing@gmail.com>
2023-06-04 09:16:39 +0200

    bug fixed: if user has already authority -> do nothing

--- src/main/java/wsb2023/pogorzelski/services/AuthorityService.java
+++ src/main/java/wsb2023/pogorzelski/services/AuthorityService.java
@@ -3,15 +3,20 @@ package wsb2023.pogorzelski.services;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import wsb2023.pogorzelski.models.Authority;
+import wsb2023.pogorzelski.models.Person;
 import wsb2023.pogorzelski.repositories.AuthorityRepository;
+import wsb2023.pogorzelski.repositories.PersonRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class AuthorityService {
 
     final private AuthorityRepository authorityRepository;
+    final private PersonRepository personRepository;
 
 
     public List<Authority> findAll(){
@@ -19,6 +24,12 @@ public class AuthorityService {
     };
 
     public void addRoleToUser(Long userId, Long authorityId){
+        Person person = personRepository.findById(userId).orElseThrow();
+        Authority authority = authorityRepository.findById(authorityId).orElseThrow();
+        Set<Authority> authorities = person.getAuthorities();
+        if(authorities.contains(authority)){
+            return;
+        }
         authorityRepository.setAuthorityToUser(userId,authorityId);
     }
 
