82c123c7c8f42e3a0298c59b7fb54028b8ee38fe
user.email <lukas.straubing@gmail.com>
2023-06-24 22:21:25 +0200

    add and remove role from user are now secured with role_manage_users authority

--- src/main/java/wsb2023/pogorzelski/controllers/AuthorityController.java
+++ src/main/java/wsb2023/pogorzelski/controllers/AuthorityController.java
@@ -1,6 +1,7 @@
 package wsb2023.pogorzelski.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +17,14 @@ public class AuthorityController {
 final private AuthorityService authorityService;
 
     @PostMapping("/{userId}")
+    @Secured("ROLE_MANAGE_USERS")
     public String addRoleToUser(@PathVariable Long userId, @RequestParam("roleId") Long roleId){
     authorityService.addRoleToUser(userId,roleId);
     return "redirect:/person/"+userId;
     };
 
     @GetMapping("/{userId}/{roleName}")
+    @Secured("ROLE_MANAGE_USERS")
     public String removeRoleFromUser(@PathVariable Long userId, @PathVariable("roleName") String roleName){
         authorityService.removeRoleFromUser(userId,roleName);
 
